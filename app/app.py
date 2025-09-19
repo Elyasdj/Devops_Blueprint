71c2a4cb72cd082438c1c6b21f947c19cb80a696
anisa <edu@anisa.co.ir>
2025-09-19 17:59:07 +0000

    edit app/app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from prometheus_client import Counter, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('http_requests_total', 'Total number of HTTP requests')
+
+@app.route('/')
+def hello_world():
+    REQUEST_COUNT.inc() # Increment the counter on each request
+    return 'Hello, DevOps Engineer!'
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
